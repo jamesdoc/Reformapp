3d0e73409f0c53c0c72c21404b81b8ac2cd3f269
Doc <hi@jamesdoc.com>
2014-12-07 16:00:07 +0000

    Add in cleverness to redirect

--- www/index.php
+++ www/index.php
@@ -8,19 +8,33 @@
 	include_once('config.inc');
 	include_once('apps.inc');
 	
-	if(isset($_GET['app'])){
+	// We are going to do a lot of date based goodness
+	$today = date('Y-m-d');
+	
+	// Right, has an app been requested?
+	if (isset($_GET['app'])) {
 		print $_GET['app'];
 		exit;
 	}
-			
-	$today = date('Y-m-d');
-	$app = find_app_in_array($apps, $today);
 	
-	if ($app) {
+	
+	
+	
+	
+	
+	
+	
+	// We've not requested an app... should we be showing one today?
+	$app = find_app_in_array($apps, $today);
+	if ($app){
 		Redirect($base_url . $app['slug'], false);
 	}
 	
+	// We've not requested an app, and there is nothing to show so, lets just return the standard index page!
 	include_once('index.htm');
+	
+	
+	
 	
 	function find_app_in_array($app_array, $today){
 		foreach ($app_array as $key => $value){
